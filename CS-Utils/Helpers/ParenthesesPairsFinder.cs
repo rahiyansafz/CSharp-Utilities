0defa1f24d27e4e87115974943c24fe85078612d
rahiyansafin <rahiyan@binaryquest.com>
2023-03-19 15:10:39 +0600

    parenthesis pair class and path utility class added

---
+++ CS-Utils/Helpers/ParenthesesPairsFinder.cs
@@ -0,0 +1,43 @@
+﻿namespace CS_Utils.Helpers;
+public static class ParenthesesPairsFinder
+{
+    public static IEnumerable<ParenthesesPair> ParseParenthesesPairs(string text, char[]? pairs = null)
+    {
+        if (pairs is not null && pairs.Length is not 2)
+            throw new Exception($"{nameof(pairs)} should contain only two items.");
+
+        if (pairs is not null && pairs.Contains(' '))
+            throw new Exception($"{nameof(pairs)} should not contain whitespace.");
+
+        if (pairs is not null && pairs[0] == pairs[1])
+            throw new Exception($"{nameof(pairs)} should not contain same value.");
+
+        pairs ??= new[] { '(', ')' };
+        var result = new List<ParenthesesPair>();
+
+        var startPositions = new Stack<int>();
+        var id = 0;
+        for (var index = 0; index < text.Length; index++)
+        {
+            if (text[index] == pairs[0])
+                startPositions.Push(index);
+            else if (text[index] == pairs[1] && startPositions.Count == 0)
+                throw new ArgumentException($"Mismatched end at index {index}");
+            else if (text[index] == pairs[1])
+            {
+                id++;
+                var depth = startPositions.Count - 1;
+                var start = startPositions.Pop();
+                var length = index - start + 1;
+                var val = text.Substring(start, length);
+
+                result.Add(new ParenthesesPair(id, start, index, depth, val));
+            }
+        }
+
+        if (startPositions.Count > 0)
+            throw new ArgumentException($"Mismatched start, {startPositions.Count} total");
+
+        return result.OrderBy(x => x.Depth).ThenBy(x => x.StartIndex);
+    }
+}
---
+++ CS-Utils/Helpers/PathUtility.cs
@@ -0,0 +1,81 @@
+﻿namespace CS_Utils.Helpers;
+public static class PathUtility
+{
+    public static bool EvaluatePath(string path, bool isFilePath = true)
+    {
+        try
+        {
+            var folderPath = isFilePath ? Path.GetDirectoryName(path) : path;
+            if (!Directory.Exists(folderPath) && folderPath != null)
+                Directory.CreateDirectory(folderPath);
+            return true;
+        }
+        catch
+        {
+            // ignored
+        }
+        return false;
+    }
+
+    public static string? GetDirectoryName(string path)
+    {
+        if (IsDirectory(path).HasValue)
+            return Path.GetFullPath(path).Split(Path.DirectorySeparatorChar).LastOrDefault();
+        var newPath = Path.GetDirectoryName(path);
+        return Path.GetFullPath(newPath ?? string.Empty).Split(Path.DirectorySeparatorChar).LastOrDefault();
+    }
+
+    public static string GetFilePathWithoutExtension(string path)
+    {
+        return Path.ChangeExtension(path, null);
+    }
+
+    public static string GetFullPathWithoutExtension(string path)
+    {
+        return Path.Combine(Path.GetDirectoryName(path) ?? string.Empty, Path.GetFileNameWithoutExtension(path));
+    }
+
+    public static string? GetParentDirectory(string path)
+    {
+        return Path.GetDirectoryName(path.Trim(Path.DirectorySeparatorChar));
+    }
+
+    // Null means there is no file or directory with this path
+    public static bool? IsDirectory(string path)
+    {
+        if (Directory.Exists(path))
+            return true;
+        if (File.Exists(path))
+            return false;
+        return null;
+    }
+
+    // Null means there is no file or directory with this path
+    public static bool? IsFile(string path)
+    {
+        var isDir = IsDirectory(path);
+        // ReSharper disable once MergeConditionalExpression
+        return isDir == null ? null : !isDir;
+    }
+
+    public static bool IsValidPath(this string path, bool allowRelativePaths = false)
+    {
+        bool isValid;
+        try
+        {
+            var _ = Path.GetFullPath(path);
+            if (allowRelativePaths)
+                isValid = Path.IsPathRooted(path);
+            else
+            {
+                var root = Path.GetPathRoot(path);
+                isValid = string.IsNullOrEmpty(root?.Trim('\\', '/')) == false;
+            }
+        }
+        catch
+        {
+            isValid = false;
+        }
+        return isValid;
+    }
+}
