bc17a36e14e3a37fcec954cf8cd93bd49eb60e3c
rahiyansafin <rahiyan@binaryquest.com>
2023-03-19 15:02:30 +0600

    File Utility Class for Copy is added & MimeType Utility Class is also added

---
+++ CS-Utils/Helpers/FileUtility.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Utils.Helpers;
+public static class FileUtility
+{
+    public static void Copy(string destination, string target)
+    {
+        var directory = Path.GetDirectoryName(target);
+        Directory.CreateDirectory(directory ?? throw new InvalidOperationException());
+        File.Copy(destination, target);
+    }
+}
---
+++ CS-Utils/Helpers/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Collections;
+global using System.Collections.Immutable;
+global using System.ComponentModel;
+global using System.Data;
+global using System.Linq.Expressions;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
